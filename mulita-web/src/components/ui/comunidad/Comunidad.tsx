1ed5d6f41c7c31d822dcef02e29914588df8288b
Nuria Desteffani <nuria.desteffani@uap.edu.ar>
2025-11-19 02:32:19 -0300

    Cambios en la pagina comunidad para que funcione el deploy

---
+++ mulita-web/src/components/ui/comunidad/Comunidad.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import { SidebarComunidad } from "@/components/ui/comunidad/SidebarComunidad";
+import Actividades from "./Actividades"
+
+export default function Comunidad() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="flex bg-gray-50 min-h-screen">
+      {/* Sidebar */}
+      <SidebarComunidad isOpen={isOpen} onClose={() => setIsOpen(false)} />
+
+      {/* Contenedor principal con un único scroll */}
+      <main className="flex-1 overflow-auto p-6 ml-0 md:ml-60">
+        {/* Botón para abrir sidebar en mobile */}
+        <button
+          onClick={() => setIsOpen(true)}
+          className="md:hidden mb-4 px-4 py-2 rounded-md bg-[#003c71] text-white font-semibold shadow-sm hover:bg-[#002a50] active:bg-[#001c36] transition-colors duration-200"
+        >
+          ☰ Menú
+        </button>
+
+        <Actividades />
+      </main>
+    </div>
+  );
+}
