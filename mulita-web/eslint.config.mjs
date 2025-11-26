e5e569948826e00eb887dacc200fcdc9e41203ae
Nuria Desteffani <nuria.desteffani@uap.edu.ar>
2025-11-26 16:49:36 -0300

    cambios en archivo eslint

--- mulita-web/eslint.config.mjs
+++ mulita-web/eslint.config.mjs
@@ -1,7 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import { rule } from "postcss/lib/postcss";
+import pkg from 'postcss/lib/postcss';
+const { rule } = pkg;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
